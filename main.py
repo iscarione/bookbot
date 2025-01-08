c93a525d53f62d5f7c57558dc57429284fd4409e
iscarione <iscario@tlen.pl>
2025-01-08 23:36:38 +0100

    count words, chars

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 def main():
     path = "./books/frankenstein.txt"
-    with open(path) as f:
-        # print( f.read() )
-        print( count_words(f.read()) )
+    text = get_text(path)
+    words = count_words(text)
+    characters = count_characters(text)    
+    print( characters )
     
 
-def count_words(string):
-    return len(string.split())
+def get_text(path):
+    with open(path) as f:
+        return f.read()
 
+def count_words(text):
+    return len(text.split())
+
+def count_characters(text):
+    alphabet = { }
+    lowered = text.lower()
+    for char in lowered:
+        if char in alphabet:
+            alphabet[char] += 1
+        else:
+            alphabet[char] = 1
+    return alphabet
 
 main()
